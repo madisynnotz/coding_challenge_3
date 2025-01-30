364b1fe1f1b8bf11fc98485f24278574ae9aecdd
Madisyn Notz <madisynnotz33@gmail.com>
2025-01-30 16:29:49 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// Task 1: Product Price Management
+
+let prices = [11, 15.49, 9.38, 11.50, 15.99]; // Declare an array with at least five numerical values
+
+prices.push(16); // Add a new price to the array
+
+prices.shift(); // Remove the first price from the array
+
+console.log(prices); // Log the updated price list to the console
